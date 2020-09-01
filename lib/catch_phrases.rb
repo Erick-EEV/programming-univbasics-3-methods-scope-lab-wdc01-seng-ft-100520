7b50660515dab13a0a4eb1f7b9183f977ae2d97a
Erick Villeda <eeverickeev@gmail.com>
2020-09-01 18:12:23 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,9 @@
 def mario
- 
   phrase = "It's-a me, Mario!"
-  puts phrase status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts phrase 
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+  phrase status
 end
 
 def toadstool
